811c83173b5fcd10f257d4e9f48f0eac1072c1d0
Daniel Griswold <daniel@griswoldcomputing.com>
2018-05-15 01:17:31 -0400

    config.sh to perform raspi-config tasks

---
+++ config.sh
@@ -0,0 +1,13 @@
+# Remote Access
+sudo raspi-config nonint do_ssh 0
+
+# Hardware Settings
+sudo raspi-config nonint do_memory_split 16
+
+# Localization
+sudo raspi-config nonint do_wifi_country US
+sudo raspi-config nonint do_configure_keyboard us
+sudo raspi-config nonint do_change_timezone America/Detroit
+sudo raspi-config nonint do_change_locale en_US.UTF8
+
+
